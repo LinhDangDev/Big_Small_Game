cf34ca9482b80db30452944d9639045a00091a24
DEV\Linh <duylinh05030723@gmail.com>
2023-05-25 21:42:22 +0700

    config condition

--- src/BigGame/BigSmallGame.java
+++ src/BigGame/BigSmallGame.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class BigSmallGame {
 
     public static void main(String[] args) throws IllegalArgumentException {
-        int playerWalletStart = -1;
-        int houseWalletStart = 1000;
+        int playerWalletStart = 0;
+        int houseWalletStart = 1000000;
         Player player = new Player();
         House house = new House();
         Scanner sc = new Scanner(System.in);
         house.addWallet (houseWalletStart);
         boolean choose = true;
         int round = 1;
+
 
         while (playerWalletStart <= 0) {
             try {
@@ -38,13 +39,21 @@ public class BigSmallGame {
             double bet = 0;
 
             do {
+                try{
                 System.out.println("How much do you want to bet?");
                 player.setBet(Integer.parseInt(sc.nextLine()));
-                if (player.getBet() > player.getWallet ()) {
-                    System.out.println("The amount you bet is higher than the money you have. Enter again!");
+                    if (player.getBet() > player.getWallet()) {
+                        throw new IllegalArgumentException("The amount you bet is higher than the money you have. Enter again!");
+                    }
+                }catch (NumberFormatException ignored) {
+                    System.out.println("Invalid number. Please enter the amount available again.");
+                } catch (IllegalArgumentException ignored) {
+                    System.out.println(ignored.getMessage());
                 }
             } while (player.getBet() > player.getWallet ());
+
             System.out.println("You have bet $" + player.getBet());
+
 
             String choice = "";
             do {
@@ -52,7 +61,7 @@ public class BigSmallGame {
                 choice = sc.nextLine();
                 player.setChoice(choice);
                 if (!player.getChoice().equals("small") && !player.getChoice().equals("big")) {
-                    System.out.println("Invalid choice. Please enter a valid choice.");
+                    throw new IllegalArgumentException ("Invalid choice. Please enter a valid choice.");
                 }
             } while (!player.getChoice().equals("small") && !player.getChoice().equals("big"));
 
@@ -61,24 +70,29 @@ public class BigSmallGame {
 
             System.out.println("The sum of 3 dices is " + house.sumDices() + "!");
 
-            int sumDices = house.sumDices();
 
-            if (sumDices >= 4 && sumDices <= 10 && player.getChoice().equals("small")) {
-                System.out.println("You won $" + player.getBet());
-                player.setWallet(player.getWallet() + player.getBet());
-                house.subtractWallet(player.getBet());
-            } else if (sumDices >= 11 && sumDices <= 17 && player.getChoice().equals("big")) {
-                System.out.println("You won $" + player.getBet());
-                player.setWallet(player.getWallet() + player.getBet());
-                house.subtractWallet(player.getBet());
-            } else if (sumDices == 3 || sumDices == 18 && player.getChoice().equals("same")) {
-                System.out.println("You lose $" + player.getBet());
-                player.setWallet(player.getWallet() - player.getBet());
-                house.addWallet(player.getBet());
+            //handle bet player
+            int sum = house.sumDices();
+            if ((sum >= 4 && sum <= 10) && player.getChoice().equals("small")) {
+                System.out.println("Congratulations! You win!");
+                int winnings = player.getBet();
+                player.setWallet (winnings);
+                house.subtractWallet(winnings);
+            } else if ((sum >= 11 && sum <= 17) && player.getChoice().equals("big")) {
+                System.out.println("Congratulations! You win!");
+                int winnings = player.getBet();
+                player.setWallet (winnings);
+                house.subtractWallet(winnings);
+            } else {
+                System.out.println("Sorry, you lose!");
+                int losses = player.getBet();
+                player.subtractWallet(losses);
+                house.addWallet(losses);
             }
 
-            System.out.println("The house has $" + house.getWallet());
-            System.out.println("The player has $" + player.getWallet());
+
+            System.out.println("The house after play game has $" + house.getWallet());
+            System.out.println("The player after play game has $" + player.getWallet());
 
             if (player.getWallet() == 0) {
                 System.out.println("You are out of money!");
@@ -93,7 +107,8 @@ public class BigSmallGame {
             System.out.println("Do you still want to continue playing? (true/false)");
             choose = sc.nextBoolean();
             sc.nextLine(); // Consume newline character
-            round++;
+
+            round++; //  round value has increase 1
 
         } while (choose);
     }
